b55074ce4a280b7679d286c3af8c9b24a23f1d64
Anna Baburina <4nna.baburina@gmail.com>
2022-01-01 18:23:35 +0500

    ADD solutions to chapter 9

--- 9_self_join.sql
+++ 9_self_join.sql
@@ -81,3 +81,63 @@ AND b.stop = 137;
 
 -- Give a list of the services which connect the stops 'Craiglockhart' and
 -- 'Tollcross'.
+SELECT  DISTINCT a.company
+       ,a.num
+FROM route AS a
+JOIN route AS b
+ON (a.company = b.company AND a.num = b.num)
+JOIN stops AS stopa
+ON (a.stop = stopa.id)
+JOIN stops AS stopb
+ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart'
+AND stopb.name = 'Tollcross';
+
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart'
+-- by taking one bus, including 'Craiglockhart' itself, offered by the LRT
+-- company. Include the company and bus no. of the relevant services.
+SELECT  stopsb.name
+       ,routeb.company
+       ,routeb.num
+FROM route AS routea
+JOIN route AS routeb
+ON (routea.num = routeb.num AND routeb.company = routea.company)
+JOIN stops AS stopsa
+ON stopsa.id = routea.stop
+JOIN stops AS stopsb
+ON stopsb.id = routeb.stop
+WHERE routea.company = 'LRT'
+AND stopsa.name = 'Craiglockhart'
+
+-- Find the routes involving two buses that can go from Craiglockhart to Lochend.
+-- Show the bus no. and company for the first bus, the name of the stop for the
+-- transfer, and the bus no. and company for the second bus.
+SELECT  first_bus.num
+       ,first_bus.company
+       ,name
+       ,second_bus.num
+       ,second_bus.company
+FROM
+(
+	SELECT  routea.num
+	       ,routea.company
+	       ,routeb.stop
+	FROM route AS routea
+	JOIN route AS routeb
+	ON (routea.num = routeb.num AND routea.company = routeb.company)
+	WHERE routea.stop = 53 
+) AS first_bus
+JOIN
+(
+	SELECT  routed.num
+	       ,routed.company
+	       ,routec.stop
+	FROM route AS routec
+	JOIN route AS routed
+	ON (routec.num = routed.num AND routec.company = routed.company)
+	WHERE routed.stop = 147 
+) AS second_bus
+ON first_bus.stop = second_bus.stop
+JOIN stops
+ON first_bus.stop = stops.id
+ORDER BY first_bus.num, name, second_bus.num;
